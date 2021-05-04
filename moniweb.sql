28217be762c89039b73177411b906e08c44db4b8
jina <jabaek92@naver.com>
2021-05-04 14:37:42 +1000

    inserted an initial dataset

--- moniweb.sql
+++ moniweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3308
--- Generation Time: May 03, 2021 at 09:40 AM
+-- Generation Time: May 04, 2021 at 04:35 AM
 -- Server version: 8.0.18
 -- PHP Version: 7.4.0
 
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `actionlog` (
   `LogId` int(11) UNSIGNED NOT NULL,
   `ClientIP` varchar(30) NOT NULL,
-  `SessionId` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `SessionId` varchar(100) DEFAULT NULL,
   `Username` varchar(50) NOT NULL,
-  `PlanType` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `PlanType` varchar(30) DEFAULT NULL,
   `LogTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `Action` varchar(50) NOT NULL,
   `ResponseCode` int(4) UNSIGNED DEFAULT NULL
@@ -43,11 +43,12 @@ CREATE TABLE `actionlog` (
 --
 
 INSERT INTO `actionlog` (`LogId`, `ClientIP`, `SessionId`, `Username`, `PlanType`, `LogTime`, `Action`, `ResponseCode`) VALUES
-(1, '::1', 'rpi0thf2p0m85lh2ee0lji38be', '', NULL, '2021-05-03 06:40:48', 'isloggedin', 401),
-(2, '::1', 'rpi0thf2p0m85lh2ee0lji38be', '', NULL, '2021-05-03 06:40:50', 'isloggedin', 401),
-(3, '::1', 'rpi0thf2p0m85lh2ee0lji38be', '', NULL, '2021-05-03 06:40:50', 'isloggedin', 401),
-(4, '::1', 'rpi0thf2p0m85lh2ee0lji38be', '', NULL, '2021-05-03 06:40:51', 'isloggedin', 401),
-(5, '::1', 'rpi0thf2p0m85lh2ee0lji38be', '', NULL, '2021-05-03 06:44:56', 'isloggedin', 401);
+(1, '::1', '05mrusp2tsorv2rjp28eb4hck0', 'test11@email.com', 'Free', '2021-05-04 04:33:47', 'register', 201),
+(2, '::1', '05mrusp2tsorv2rjp28eb4hck0', 'test11@email.com', 'Free', '2021-05-04 04:33:47', 'register', 206),
+(3, '::1', '05mrusp2tsorv2rjp28eb4hck0', 'test11@email.com', 'Free', '2021-05-04 04:34:13', 'login', 401),
+(4, '::1', '05mrusp2tsorv2rjp28eb4hck0', 'test11@email.com', 'Free', '2021-05-04 04:34:13', 'login', 401),
+(5, '::1', '05mrusp2tsorv2rjp28eb4hck0', 'test11@email.com', 'Free', '2021-05-04 04:34:36', 'login', 200),
+(6, '::1', '05mrusp2tsorv2rjp28eb4hck0', 'test11@email.com', 'Free', '2021-05-04 04:34:36', 'login', 200);
 
 -- --------------------------------------------------------
 
@@ -67,9 +68,16 @@ CREATE TABLE `customer` (
   `State` varchar(15) DEFAULT NULL,
   `Postcode` varchar(10) DEFAULT NULL,
   `Country` varchar(25) NOT NULL,
-  `Theme` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Light',
+  `Theme` varchar(10) NOT NULL DEFAULT 'Light',
   `RegisterDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`CustomerId`, `Email`, `Password`, `FirstName`, `LastName`, `PhoneNo`, `Address`, `Suburb`, `State`, `Postcode`, `Country`, `Theme`, `RegisterDate`) VALUES
+(1, 'test11@email.com', '$2y$10$Skz8yeqyAvkz5TVO5dNdLudXfeRzTJFUn5KQCuIWFt.tHw6c92iMO', 'test', 'test', '+11222333', 'test', 'test', 'QLD', '', 'Australia', 'Light', '2021-05-04');
 
 -- --------------------------------------------------------
 
@@ -88,6 +96,14 @@ CREATE TABLE `customerplan` (
   `PlanId` int(5) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `customerplan`
+--
+
+INSERT INTO `customerplan` (`CustomerPlanId`, `CustomerId`, `paymentId`, `PaidDate`, `Price`, `StartDate`, `EndDate`, `PlanId`) VALUES
+(1, 3, NULL, NULL, NULL, '2021-05-04', NULL, 1),
+(2, 1, NULL, NULL, NULL, '2021-05-04', NULL, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -101,6 +117,17 @@ CREATE TABLE `customerplanurl` (
   `CustomerPlanId` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `customerplanurl`
+--
+
+INSERT INTO `customerplanurl` (`UrlId`, `URL`, `IPaddress`, `CustomerPlanId`) VALUES
+(1, NULL, NULL, 2),
+(2, NULL, NULL, 2),
+(3, NULL, NULL, 2),
+(4, NULL, NULL, 2),
+(5, NULL, NULL, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -109,9 +136,9 @@ CREATE TABLE `customerplanurl` (
 
 CREATE TABLE `monitorstatus` (
   `StatusId` int(11) UNSIGNED NOT NULL,
-  `UrlId` int(11) UNSIGNED DEFAULT NULL,
-  `MonitorTime` timestamp NULL DEFAULT NULL,
-  `ResultStatus` int(3) UNSIGNED DEFAULT NULL,
+  `UrlId` int(11) UNSIGNED NOT NULL,
+  `MonitorTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `ResultStatus` int(3) UNSIGNED NOT NULL,
   `Description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -140,13 +167,22 @@ CREATE TABLE `plan` (
   `PlanId` int(5) UNSIGNED NOT NULL,
   `Type` varchar(30) DEFAULT NULL,
   `Description` varchar(200) DEFAULT NULL,
-  `MaxNumURL` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `MaxNumURL` varchar(100) DEFAULT NULL,
   `CheckFrequency` int(3) DEFAULT NULL,
   `ReportPeriod` int(2) UNSIGNED DEFAULT NULL,
   `NotificationPeriod` int(1) UNSIGNED DEFAULT NULL,
   `NotificationMethod` int(1) UNSIGNED DEFAULT '1',
   `Price` decimal(5,2) DEFAULT '0.00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `plan`
+--
+
+INSERT INTO `plan` (`PlanId`, `Type`, `Description`, `MaxNumURL`, `CheckFrequency`, `ReportPeriod`, `NotificationPeriod`, `NotificationMethod`, `Price`) VALUES
+(1, 'Free', 'Default free plan', '5', 60, 1, 1, 1, '0.00'),
+(2, 'Standard', ' Standard plan', '20', 10, 2, 2, 2, '5.00'),
+(3, 'Premium', 'Premium plan', '50', 1, 3, 3, 2, '10.00');
 
 --
 -- Indexes for dumped tables
@@ -209,25 +245,25 @@ ALTER TABLE `plan`
 -- AUTO_INCREMENT for table `actionlog`
 --
 ALTER TABLE `actionlog`
-  MODIFY `LogId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `LogId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `CustomerId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `CustomerId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `customerplan`
 --
 ALTER TABLE `customerplan`
-  MODIFY `CustomerPlanId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `CustomerPlanId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customerplanurl`
 --
 ALTER TABLE `customerplanurl`
-  MODIFY `UrlId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `UrlId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `monitorstatus`
